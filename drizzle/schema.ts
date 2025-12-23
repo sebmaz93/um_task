fbbe1bc49a14000e47ef9f41b5a7ef11acab5569
sebmaz <seb.maz93@icloud.com>
2025-12-23 17:13:22 +0400

    Add schema and env

---
+++ drizzle/schema.ts
@@ -0,0 +1,43 @@
+import {
+  pgTable,
+  serial,
+  varchar,
+  timestamp,
+  foreignKey,
+  integer,
+} from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+
+export const users = pgTable("users", {
+  id: serial().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at", { mode: "string" }),
+});
+
+export const userGroups = pgTable(
+  "user_groups",
+  {
+    userId: integer("user_id").notNull(),
+    groupId: integer("group_id").notNull(),
+  },
+  (table) => [
+    foreignKey({
+      columns: [table.userId],
+      foreignColumns: [users.id],
+      name: "user_groups_user_id_fkey",
+    }).onDelete("cascade"),
+    foreignKey({
+      columns: [table.groupId],
+      foreignColumns: [groups.id],
+      name: "user_groups_group_id_fkey",
+    }).onDelete("cascade"),
+  ],
+);
+
+export const groups = pgTable("groups", {
+  id: serial().notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  status: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at", { mode: "string" }),
+});
